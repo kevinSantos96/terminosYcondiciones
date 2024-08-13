80a0976c148a4195995ef9d14926c98245f416c8
Kevin Santos <95662792+kevinSantos96@users.noreply.github.com>
2024-08-12 19:26:19 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,6 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
         <NavigationRoute />
